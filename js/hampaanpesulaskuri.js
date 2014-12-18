127a296af029c63caa820dc3145c4515cf473acb
Joona Somerkivi <joona.somerkivi@solinor.com>
2014-12-19 00:02:50 +0200

    Added about-modal

--- js/hampaanpesulaskuri.js
+++ js/hampaanpesulaskuri.js
@@ -104,7 +104,7 @@ function fireConfetti()
 var fanfare;
 
 soundManager.setup({
-    url: 'soundmanaager/',
+    url: 'soundmanager/',
     flashVersion: 9, // optional: shiny features (default = 8)
     // optional: ignore Flash where possible, use 100% HTML5 mode
     preferFlash: false,
